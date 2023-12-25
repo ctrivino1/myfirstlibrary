2d8c7b022ae45dc9ed12d70a11dba3eaa65c63a4
Chris Trivino <56085629+ctrivino1@users.noreply.github.com>
2023-12-24 19:36:22 -0700

    Update global_variable_advanced.py

--- function_folder/global_variable_advanced.py
+++ function_folder/global_variable_advanced.py
@@ -8,35 +8,35 @@ class GlobalVariables:
         self.value1 = None
         self.advanced_variable = None
 
-def run_all_functions():
-    global all_globals
+    def run_all_functions():
+        global all_globals
+        # Call all functions and update global variables
+        function1()
+        function2()
+        function3()
+        function4()
+        print('finished')
 
-    # Call all functions and update global variables
-    function1()
-    function2()
-    function3()
-    function4()
+    def function1():
+        # Update global variables in function1
+        global all_globals
+        all_globals.dataset1 = [1, 2, 3, 4, 5]
 
-def function1():
-    # Update global variables in function1
-    global all_globals
-    all_globals.dataset1 = [1, 2, 3, 4, 5]
+    def function2():
+        # Update global variables in function2
+        global all_globals
+        all_globals.column_name = 'example_column'
 
-def function2():
-    # Update global variables in function2
-    global all_globals
-    all_globals.column_name = 'example_column'
+    def function3():
+        # Update global variables in function3
+        global all_globals
+        all_globals.value1 = 42
 
-def function3():
-    # Update global variables in function3
-    global all_globals
-    all_globals.value1 = 42
-
-def function4():
-    # Update global variables in function4 (create a Pandas DataFrame)
-    global all_globals
-    data = {'A': [1, 2, 3], 'B': [4, 5, 6]}
-    all_globals.advanced_variable = pd.DataFrame(data)
+    def function4():
+        # Update global variables in function4 (create a Pandas DataFrame)
+        global all_globals
+        data = {'A': [1, 2, 3], 'B': [4, 5, 6]}
+        all_globals.advanced_variable = pd.DataFrame(data)
 
 # Initialize the global variables object
 all_globals = GlobalVariables()
