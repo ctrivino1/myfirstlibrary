2878c64ebb78b62b7e02b7c949474a5cc1201d7d
Chris Trivino <56085629+ctrivino1@users.noreply.github.com>
2023-12-24 19:23:36 -0700

    Update global_variable_advanced.py

--- function_folder/global_variable_advanced.py
+++ function_folder/global_variable_advanced.py
@@ -1,40 +1,41 @@
+# global_variable_advanced.py
 
-import pandas as pd
-
-# List of global variables
-global_variables = {
-    'dataset1': None,
-    'column_name': None,
-    'value1': None,
-    'data_frame': None,
-}
-
-def function1():
-    # Update global variables in function1
-    global global_variables
-    global_variables['dataset1'] = [1, 2, 3, 4, 5]
-
-def function2():
-    # Update global variables in function2
-    global global_variables
-    global_variables['column_name'] = 'example_column'
-
-def function3():
-    # Update global variables in function3
-    global global_variables
-    global_variables['value1'] = 42
-
-def function4():
-    # Update global variables in function4 (create a Pandas DataFrame)
-    global global_variables
-    data = {'A': [1, 2, 3], 'B': [4, 5, 6]}
-    global_variables['data_frame'] = pd.DataFrame(data)
+class GlobalVariables:
+    def __init__(self):
+        self.dataset1 = None
+        self.column_name = None
+        self.value1 = None
+        self.advanced_variable = None
 
 def run_all_functions():
-    # Run all functions
+    global all_globals
+
+    # Call all functions and update global variables
     function1()
     function2()
     function3()
     function4()
 
-# You can add more functions as needed
+def function1():
+    # Update global variables in function1
+    global all_globals
+    all_globals.dataset1 = [1, 2, 3, 4, 5]
+
+def function2():
+    # Update global variables in function2
+    global all_globals
+    all_globals.column_name = 'example_column'
+
+def function3():
+    # Update global variables in function3
+    global all_globals
+    all_globals.value1 = 42
+
+def function4():
+    # Update global variables in function4 (create a Pandas DataFrame)
+    global all_globals
+    data = {'A': [1, 2, 3], 'B': [4, 5, 6]}
+    all_globals.advanced_variable = pd.DataFrame(data)
+
+# Initialize the global variables object
+all_globals = GlobalVariables()
